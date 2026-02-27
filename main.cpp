6b2d69610232c65af05d3d8d0f1f409c1f497d78
zhuzk <3304239857@qq.com>
2026-02-27 16:21:10 +0800

    recover main,, nothing to attend on

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<vector>
+#include"Huffman.h"
+using namespace std;
+//constexpr int test(int t);
+
+int main(){
+    std::cout<<"Hello World!"<<std::endl;
+    string s="aabbbccddd";
+    std::cout<<s<<std::endl;
+
+    Huffman hfm(s);
+    hfm.printTree();
+    hfm.printCodes();
+    return 0;
+}
